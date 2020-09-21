809cdc9b9dbde0d0e8f7bd13911648cca74ee472
luqihao <1021008256@qq.com>
2020-09-21 09:13:55 +0800

    chore: composition RESOLVE_DELAY

--- src/component/handlers/composition/DraftEditorCompositionHandler.js
+++ src/component/handlers/composition/DraftEditorCompositionHandler.js
@@ -34,7 +34,7 @@ const isSelectionAtLeafStart = require('isSelectionAtLeafStart');
  * leads to composed characters being resolved and re-render occurring
  * sooner than we want.
  */
-const RESOLVE_DELAY = 50;
+const RESOLVE_DELAY = 0;
 
 /**
  * A handful of variables used to track the current composition and its
